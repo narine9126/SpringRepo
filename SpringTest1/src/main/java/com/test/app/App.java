3ef88c442a97d49991efddfe1e9142a2da00ab2e
narine9126 <narine9126@gmail.com>
2021-11-21 04:28:40 +0530

    sop statement modified on 21-11-2021

--- SpringTest1/src/main/java/com/test/app/App.java
+++ SpringTest1/src/main/java/com/test/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Welcome to Git" );
+        System.out.println( "Hello Welcome to Git first time" );
     }
 }
